d69d703ed333b506c4612b61cd6676f17157f6ca
LoTuS-SK <109459024+LoTuS-SK@users.noreply.github.com>
2022-11-16 11:00:56 +0300

    form size

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import "./form.css"
 
 export const Form = () => {
+
+    const [name,setname] = useState("Sergey")
+    const [number,setnamber]= useState(0)
+
+    const onchane =(e)=>{
+        setname(e.target.value)
+    }
+
   return (
-    <div>
-        <input type="text" className='form'placeholder='Имя'/>
-        <input type="number" className='form' placeholder='количество гостей'/>
+    <div className={'form'}>
+        <input 
+        type="text" 
+        className='input'
+        placeholder='Имя'
+        value={name}
+        onChange={onchane}
+        
+        />
+        <input type="number" className={'input'} placeholder='количество гостей'/>
     </div>
   )
 }
