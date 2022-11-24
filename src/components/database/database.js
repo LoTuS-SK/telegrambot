a7ce48ab3c901aea58275b1b2b8d16fa6af9a56d
LoTuS-SK <109459024+LoTuS-SK@users.noreply.github.com>
2022-11-24 12:08:38 +0300

    new list

---
+++ src/components/database/database.js
@@ -0,0 +1,16 @@
+export const base = [
+    {id:1,name:"Филе Енисейского малосолёного муксуна",price:490,weight:100,section:"закуски"},
+    {id:2,name:"Икра щуки/омуля/нерки с румяными тостами и сливочным маслом",price:490,weight:100,section:"закуски"},
+    {id:3,name:"Омуль малосоленый",price:490,weight:100,section:"закуски"},
+    {id:4,name:"Слабосолёная Карельская форель с мочёной брусникой",price:490,weight:100,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+    {id:5,name:"Строганина из чира",price:460,weight:60,section:"закуски"},
+
+]
--- src/components/productlist/product.jsx
+++ src/components/productlist/product.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
+import {base} from "../database/database"
+import { useState } from 'react'
+import "./product.css"
+
+
+//
 
 export const Product = () => {
-  return (
-    <div>product</div>
-  )
+  
+  const [items,setitems] = useState([])
+  const [display,setdisplay] = useState(true)
+  
+  function add(e){
+    
+    const text = e.target.textContent;
+
+    if(items.length===0){
+   
+    let newbludo = {name:text,count:1}
+    setitems([...items,newbludo])
+    }
+    if(items.length>=1){
+      if(items.some(el=>el.name===text)){
+        let count = items[items.findIndex(el=>el.name===text)].count
+        console.log(count)
+        let newcount = items[items.findIndex(el=>el.name===text)].count=count+1
+        console.log(newcount)
+        setitems([...items])
+
+      } else{
+        let newbludo = {name:text,count:1}
+        setitems([...items,newbludo])
+      }
+    }
+  }
+
+  const menu1 = ["Закуски","Салаты","Печь","Горячее Блюда","Гриль"]
+
+const displaySnake = () =>{
+      setdisplay(false)
 }
+
+
+return (
+    <div className={"menu"}>
+      <div className={display ? "snakes none" : "snakes" }>
+      {
+      base.map(data1=>(<button onClick={add}>{data1.name}</button>))
+      }
+      </div>
+      <div className={display ? "menu-items" : "menu-items none"}>
+            {menu1.map(element=>(<button onClick={displaySnake}>{element}</button>))}
+      </div>
+
+
+
+      {
+       items.map(el=>(<div className="d">{el.name} - {el.count}</div>))
+      }
+      
+    </div>
+  )
+       }       
+      
+     
