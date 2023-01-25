e6980e227153fb1c9174429e944bd215c813f1e6
LoTuS-SK <109459024+LoTuS-SK@users.noreply.github.com>
2023-01-25 08:46:11 +0300

    tort6

--- src/components/productlist/bar/bar.jsx
+++ src/components/productlist/bar/bar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./bar.css"
-import {drinks} from "../../database/basebar"
-import { useState } from 'react'
+//import {drinks} from "../../database/basebar"
+//import { useState } from 'react'
 
 
 export const Bar = ({add,back,display}) => {
   
-  const [drinks_dispaly,set_drinks_display]= useState(false)
+  //const [drinks_dispaly,set_drinks_display]= useState(false)
 
 
 
